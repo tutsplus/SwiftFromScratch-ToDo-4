b2825fe916fe781aaa0e2487055e7ffdfefeef9c
Bart Jacobs <developer@bartjacobs.com>
2017-04-20 08:40:41 +0200

    Project setup

---
+++ README.md
@@ -0,0 +1,9 @@
+### Envato Tuts+ Tutorial: Swift From Scratch: Inheritance and Protocols
+
+#### Instructor: Bart Jacobs
+
+While playgrounds are a great tool to play with Swift and learn the language, it's time to move on and create our first Swift project in Xcode. In this lesson, we're going to create the foundation of a simple to-do application using Xcode and Swift. Along the way, we'll learn more about object-oriented programming and we'll also take a closer look at the integration of Swift and Objective-C.
+
+Source files for the Envato Tuts+ tutorial: [Swift From Scratch: Inheritance and Protocols](https://code.tutsplus.com/tutorials/swift-from-scratch-inheritance-and-protocols--cms-23334)
+
+**Read this tutorial on [Envato Tuts+](https://code.tutsplus.com)**
--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet var tableView: UITableView!
+
+    var items: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Set Title
+        title = "To Do"
+
+        // Populate Items
+        items = ["Buy Milk", "Finish Tutorial", "Play Minecraft"]
+
+        // Register Class for Cell Reuse
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TableViewCell")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Fetch Item
+        let item = items[indexPath.row]
+
+        // Dequeue Cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+
+        // Configure Cell
+        cell.textLabel?.text = item
+        
+        return cell
+    }
 
 }
-
