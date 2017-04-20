894aada1c23859df47a7ba786e4eb1b5b6a58e3a
Bart Jacobs <developer@bartjacobs.com>
2017-04-20 08:01:00 +0200

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Bart Jacobs on 20/04/2017.
+//  Copyright © 2017 Envato Tuts+. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
